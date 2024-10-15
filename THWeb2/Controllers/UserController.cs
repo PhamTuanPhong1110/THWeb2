64154272ed02f8d31d7dcf47d381c4afc6a6b9ab
PhamTuanPhong1110 <phamtuanphongbatribentre@gmail.com>
2024-10-15 14:21:26 +0700

    v1

---
+++ THWeb2/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace THWeb2.Controllers
+{
+    public class UserController : Controller
+    {
+        // Hàm script để trong phần Body của trang HTMl
+        //Sử dụng hợp thoại Prompt để nhập liệu
+        //Sử dụng hàm document.write để hiện thị dữ liệu
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+        //Hàm script để trong phần tử body của trang HTML
+        //Sử dụng input type=text để nhập liệu
+        //Sử dụng hộp thoại Alert để hiển thị thông tin
+        public ActionResult Welcome2()
+        {
+            return View();
+        }
+        public ActionResult Welcome3()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ THWeb2/Scripts/myScript/Userscript.js
@@ -0,0 +1,11 @@
+﻿function Homnay() {
+    var date = new Date();
+    //alert("tToday is day " + date.getDay() +
+    //  "month" + date.getMonth() +
+    // "year" + date.getFulYear());
+
+
+    document.write("Today is day" + date.getDay() +
+        "month" + date.getMonth() +
+        "year" + date.getFullYear());
+}
